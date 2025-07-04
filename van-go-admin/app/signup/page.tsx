f74c8a1cc7745703c2623168dc3a37b33bdb8069
Tree11111 <heangmengtry@gmail.com>
2025-07-04 19:29:46 +0700

    First Commit

---
+++ van-go-admin/app/signup/page.tsx
@@ -0,0 +1,32 @@
+// app/signup/page.tsx
+import { Truck } from "lucide-react"
+import { SignupForm } from "@/components/signup-form"
+
+export default function SignupPage() {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="/" className="flex items-center gap-2 font-medium">
+            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+              <Truck className="size-4" />
+            </div>
+            Van Go Admin.
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-md">
+            <SignupForm />
+          </div>
+        </div>
+      </div>
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src="/asus.jpg"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover pl:10 dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  )
+}
